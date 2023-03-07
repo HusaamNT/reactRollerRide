d201fa29ee012505a3d8766375f424cbc2afa3cf
Husaam NT <husnaeem@gmail.com>
2023-03-07 14:11:50 +0000

    added pre and post password methods

--- server/models/User.js
+++ server/models/User.js
@@ -1,30 +1,44 @@
 const { Schema, Model } = require("mongoose");
 
 const userSchema = new Schema({
-    id: mongoose.Schema.Types.ObjectId,
-    firstname: {
-        type: String,
-        required: true
-    },
-    username: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [
-          /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
-          "Please fill a valid email address",
-        ],
-    },
-    password: {
-        type: String,
-        required: true
-    }
+  id: mongoose.Schema.Types.ObjectId,
+  firstname: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [
+      /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+      "Please fill a valid email address",
+    ],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
 });
 
-const User = mongoose.Model('User', userSchema);    
+profileSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+profileSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.Model("User", userSchema);
 
 module.exports = User;
