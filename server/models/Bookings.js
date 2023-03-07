63b515dbfa31b83690acebee5c320ad825d4b143
Husaam NT <husnaeem@gmail.com>
2023-03-07 18:52:16 +0000

    added all booking.js model elements

--- server/models/Bookings.js
+++ server/models/Bookings.js
@@ -1,13 +1,24 @@
 const { Schema, Model } = require("mongoose");
 
 const bookingsSchema = new Schema({
-    id: mongoose.Schema.Types.ObjectId,
-    package:{ 
-        type: String,
-        required: true
-    },
-    userId:{
+  id: mongoose.Schema.Types.ObjectId,
+  package: {
+    type: String,
+    required: true,
+  },
+  userId: {
     type: Schema.Types.ObjectId,
-    ref
-    }
-})
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAt) => {
+      return new Date(createdAt).toLocaleString();
+    },
+  },
+});
+
+const Bookings = mongoose.model('Bookings', bookingsSchema);
+
+module.exports = Bookings;
