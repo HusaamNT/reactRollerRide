7c6a304b5c29a2ee39263d01265c6cdbee2d6bed
CharlieDoll <113851328+CharlieDoll@users.noreply.github.com>
2023-03-09 21:36:33 +0000

    resolvers login and user create fixed

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,7 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
-  _id: Schema.Types.ObjectId,
-  firstname: {
-    type: String,
-    required: true,
-  },
   username: {
     type: String,
     required: true,
@@ -25,7 +21,17 @@ const userSchema = new Schema({
   },
 });
 
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
 userSchema.methods.isCorrectPassword = async function (password) {
+  console.log(this.password, password);
   return bcrypt.compare(password, this.password);
 };
 
--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,39 +1,41 @@
-const { Profile } = require('../models');
-const { signToken } = require('../utils/auth.js');
+// const { Profile } = require('../models');
+const User = require("../models/User");
+const { signToken } = require("../utils/auth.js");
 
 const resolvers = {
-    Query: {
-      users: async () => {
-        return await Profile.find()
-      },
-      login: async (_, { username, password }) => {
-        const profile = await Profile.findOne({ username });
-  
-        if (!profile) {
-          throw new AuthenticationError('No profile with this username found!');
-        }
-  
-        const correctPw = await profile.isCorrectPassword(password);
-  
-        if (!correctPw) {
-          throw new AuthenticationError('Incorrect password!');
-        }
-  
-        const token = signToken(profile);
-        return { token, profile };
+  Query: {
+    users: async () => {
+      return await User.find();
+    },
+    login: async (_, { username, password }) => {
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        throw new Error("No profile with this username found!");
       }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new Error("Incorrect password!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
     },
-  
-    Mutation: {
-      addUser: async (_, { username, email, password }) => {
-        const user = await Profile.create({
-          username, email, password
-        })
-        const token = signToken(user);
-        return { token }
-      },
+  },
+
+  Mutation: {
+    addUser: async (_, { username, email, password }) => {
+      const user = await User.create({
+        username,
+        email,
+        password,
+      });
+      const token = signToken(user);
+      return { token };
     },
-  };
-  
-  module.exports = resolvers;
-  
+  },
+};
+
+module.exports = resolvers;
