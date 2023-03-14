9f0e289ae7aac85adec1be776c7433bc4518a855
Maryan Abdalla <113936470+maryanne3@users.noreply.github.com>
2023-03-14 12:14:49 +0000

    Home & About

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,4 +1,6 @@
 import React ,{useState} from 'react';
+import {Link} from 'react-router-dom';
+
 
 
 function Navbar() {
@@ -8,11 +10,28 @@ function Navbar() {
     <>
     <nav className='navbar'>
         <div className='navbar-container'>
+            <Link to='/' className='navbar-logo'>
+                Roller Ride <i className='fa-solid fa-roller-coaster'/>
+            </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i  className={click? 'fas fa-times':'fas fa-bars'}/>
-
           </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+                <Link to ='/' className='nav-links'>
+                    Home
+                </Link>
+            </li>
+            <li className='nav-item'>
+                <Link to='/' className='nav-links'>
+                    About
+                </Link>
+            </li>
+
+
+          </ul>
             
+
             
 
         </div>
