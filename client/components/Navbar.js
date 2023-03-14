9236ee71dc5a64cce285c8857ac23a49b153b409
Maryan Abdalla <113936470+maryanne3@users.noreply.github.com>
2023-03-14 11:48:51 +0000

    menu-icon

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,14 +1,17 @@
-import React ,{useState} from 'react'
+import React ,{useState} from 'react';
+
 
 function Navbar() {
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click);
   return (
     <>
     <nav className='navbar'>
         <div className='navbar-container'>
-            <link to="/" className="navbar-logo">
-                Roller-Ride
-                
-            </link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i  className={click? 'fas fa-times':'fas fa-bars'}/>
+
+          </div>
             
             
 
